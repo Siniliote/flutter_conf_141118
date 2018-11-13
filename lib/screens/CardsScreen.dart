fa28c7eea7fe9e78ab2af116630b91a37e7f2625
Siniliote <stephane.ferey@gmail.com>
2018-11-13 22:11:50 +0100

    fix(todo): step4

--- lib/screens/CardsScreen.dart
+++ lib/screens/CardsScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/magic_card.dart';
+import '../http.dart' as rest_api;
+
 class CardsScreen extends StatefulWidget {
   final String title;
 
@@ -10,15 +13,57 @@ class CardsScreen extends StatefulWidget {
 }
 
 class _CardsScreenState extends State<CardsScreen> {
+  Future<MagicCard> magicCard;
 
-  //@TODO: 12- Fetch and Display the data
-  //@see: https://flutter.io/docs/cookbook/networking/fetch-data
-
+  @override
+  void initState() {
+    super.initState();
+    magicCard = rest_api.randomCard();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('CardsScreen'),
+        child: FutureBuilder<MagicCard>(
+          future: this.magicCard,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                padding: EdgeInsets.all(8.0),
+                child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 480.0,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned.fill(
+                              child: (snapshot.data.imageUrl != null)
+                                  ? Image.network(
+                                      snapshot.data.imageUrl,
+                                      fit: BoxFit.fill,
+                                    )
+                                  : Image.asset(
+                                      "images/Magic_Card_Back.png",
+                                      fit: BoxFit.cover,
+                                    ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
