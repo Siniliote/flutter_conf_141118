c4410d17313aa6ab0484b7b096112be9be5812ab
Stephane Ferey <ext-stephane.ferey@mister-auto.com>
2018-11-13 13:43:12 +0100

    rename page HomePage to ListingPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'pages/home.dart';
+import 'pages/listing.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Abbeal Meetup #2018'),
+      home: MyListingPage(title: 'Abbeal Meetup #2018'),
     );
   }
 }
--- lib/pages/listing.dart
+++ lib/pages/listing.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyListingPage extends StatefulWidget {
+  MyListingPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyListingPageState createState() => _MyListingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyListingPageState extends State<MyListingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
